c5a664312ae1bab56c1c23dcbae10fad411e7bec
joaoei <joaoeizidio@gmail.com>
2018-08-28 10:13:32 -0300

    Classe Quarto adicionada

---
+++ src/padrao/Quarto.java
@@ -0,0 +1,34 @@
+package padrao;
+
+public class Quarto {
+
+	private int vagas;
+	private int qtd_quartos_reservados;
+	private double preco_diaria;
+	
+	public Quarto(int vagas, int qtd_quartos_reservados, double preco_diaria) {
+		this.vagas = vagas;
+		this.qtd_quartos_reservados = qtd_quartos_reservados;
+		this.preco_diaria = preco_diaria;
+	}
+
+	public int getVagas() {
+		return vagas;
+	}
+
+	public int getQtd_quartos_reservados() {
+		return qtd_quartos_reservados;
+	}
+
+	public double getPreco_diaria() {
+		return preco_diaria;
+	}
+
+	public void reservarQuarto() {
+		this.qtd_quartos_reservados = this.qtd_quartos_reservados + 1;
+	}
+	
+	public int getQuartosDisponiveis() {
+		return getVagas() - getQtd_quartos_reservados();
+	}
+}
