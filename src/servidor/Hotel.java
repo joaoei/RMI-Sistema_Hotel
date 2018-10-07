cab65755fe45b8f4a73866f040d2cbcbdcd7c4bb
João Emmanuel <joaoeizidio@gmail.com>
2018-10-07 02:19:57 -0300

    Estrutura inicial do projeto SOAP

---
+++ src/servidor/Hotel.java
@@ -0,0 +1,13 @@
+package servidor;
+
+import java.util.ArrayList;
+
+public class Hotel {
+	private ArrayList<ArrayList<Quarto>> quartos = new ArrayList<ArrayList<Quarto>>();
+	
+	protected Hotel() { 
+		super();
+		
+		//ALGO
+	}
+}
--- src/servidor/IReserva.java
+++ src/servidor/IReserva.java
@@ -1,20 +1,18 @@
-/*
- ===============================================================================
- ARQUIVO............: IReserva.java
- DESCRICAO..........: Codigo-fonte correspondente a definicao de uma interface 
- 					  remota, que especifica os metodos a serem providos do lado
- 					  servidor.
- AUTOR..............: João Emmanuel e Irene Ginani
- ===============================================================================
-*/
-
 package servidor;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface IReserva extends Remote {
-	public ArrayList<ArrayList<Quarto>> listarQuartosDisponiveis() throws RemoteException;
-	public boolean reservarQuarto(int tipo_quarto, String nome_cliente) throws RemoteException;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public interface IReserva {
+	@WebMethod
+	public ArrayList<ArrayList<Quarto>> listarQuartosDisponiveis();
+	
+	@WebMethod
+	public boolean reservarQuarto(int tipo_quarto, String nome_cliente);
 }
