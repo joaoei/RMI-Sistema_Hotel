5b4c6a776cbac6cecdd3b954a086105794e4c90e
Wellyson Gustavo Silva Oliveira <wellysongustavo@A202-04l.imd.local>
2018-10-08 12:01:39 -0300

    modificando o quarto

--- src/servidor/Hotel.java
+++ src/servidor/Hotel.java
@@ -3,11 +3,15 @@ package servidor;
 import java.util.ArrayList;
 
 public class Hotel {
+
+	private int quantidade_quartos;
 	private ArrayList<ArrayList<Quarto>> quartos = new ArrayList<ArrayList<Quarto>>();
 	
-	protected Hotel() { 
+	// os três pontos serve para dizer que o tamanho das variáveis é flexível, podem ser 1 ou 1 milhão de tipos e precos, sendo par a par, 1
+	// preco 1 tipo. 2 preco 2 tipo e assim sucessivamente
+
+	public Hotel() { 
 		super();
 		
-		//ALGO
 	}
 }
--- src/servidor/Quarto.java
+++ src/servidor/Quarto.java
@@ -14,29 +14,36 @@ import java.io.Serializable;
 public class Quarto implements Serializable {
 
 	private boolean vago;
-	private String nome_cliente;
+	private String tipo;
 	private double preco_diaria;
 	
-	public Quarto(double preco_diaria) {
+	public Quarto(String tipo, double preco_diaria) {
 		this.vago = true;
-		this.nome_cliente = "";
+		this.tipo = tipo;
 		this.preco_diaria = preco_diaria;
-	}
-
-	public void reservarQuarto(String nome_cliente) {
-		this.nome_cliente = nome_cliente;
-		vago = false;
 	}
 
 	public boolean isVago() {
 		return vago;
 	}
 
-	public String getNome_cliente() {
-		return nome_cliente;
+	public String getTipo() {
+		return tipo;
 	}
 
 	public double getPreco_diaria() {
 		return preco_diaria;
+	}
+
+	public void setVago(boolean vago) {
+		this.vago = vago;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void getPreco_diaria(double preco_diaria) {
+		this.preco_diaria = preco_diaria;
 	}
 }
